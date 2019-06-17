ee70ad53928d464bf17bdd0e64975d00a1cd0830
athnath <32517652+athnath@users.noreply.github.com>
2019-06-17 09:19:19 +0530

    Add roadcuase

--- Backend/controllers/roadcause.controller.js
+++ Backend/controllers/roadcause.controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const accidentmodel = require('../models/roadcause.model');
 
-//insert accident data
+//insert accident data and images
 router.post('/', accidentmodel.insertAccident)
 
 module.exports = router;
