52d8bd0c6f11ae020d0741b1fc7ce377e66bbadb
vediras <ashene.edirisinghe@pearson.com>
2019-06-25 15:19:53 +0530

    lisence id extraction modified Tesseract 4.0

--- Ashane/ID_License_validation/Document_Validation_Final/Read_Lisence/face_detect_lisence.py
+++ Ashane/ID_License_validation/Document_Validation_Final/Read_Lisence/face_detect_lisence.py
@@ -1,12 +1,14 @@
 import cv2
 import sys
 import imutils
+from PIL import Image
 
 # Path of working folder on Disk
 #src_path = "tes-img/"
 
 def checkFaces(img_path):
     # Get user supplied values
+    picture= Image.open(img_path)
     imagePath = img_path
     cascPath = "haarcascade_frontalface_default.xml"    
 
@@ -27,13 +29,14 @@ def checkFaces(img_path):
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30)
-        #flags = cv2.CV_HAAR_SCALE_IMAGE
     )
 
     print("Found {0} faces!".format(len(faces)))
 
     if(len(faces)!=1):
         print("Please insert a valid image")
+        picture.rotate(90, expand=True).save(img_path)
+        checkFaces(img_path)
     else:
         print("Image verified")
 
@@ -43,9 +46,6 @@ def checkFaces(img_path):
 
     # cv2.imshow("Faces found", image)
     # cv2.waitKey(0)
-    return len(faces)
+        return len(faces)
 
-#Calling the methods
-#print("--Started image processing for lisence--")
-#checkFaces(src_path + "lisence.jpg")
 
