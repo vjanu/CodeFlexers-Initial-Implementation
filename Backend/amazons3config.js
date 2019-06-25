19ba6733deb14fdb619f0d44042f2bba52e4e8df
athnath <32517652+athnath@users.noreply.github.com>
2019-06-25 07:32:22 +0530

    change access key

--- Backend/amazons3config.js
+++ Backend/amazons3config.js
@@ -2,8 +2,13 @@ const AWS = require('aws-sdk');
 
 //configuring the AWS environment
 AWS.config.update({
-    accessKeyId: "AKIAJES4NA2UXSQVD5VQ",
-    secretAccessKey: "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"
+//    accessKeyId: "AKIAJES4NA2UXSQVD5VQ",
+//    secretAccessKey: "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"
+  accessKeyId: "AKIAIP2NDEXIICSEBBCA",
+    secretAccessKey: "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"
+
+
+
   });
 
 module.exports = AWS;
